bd6597bbedcaf79fdf41d05aef4cfd5a328c2363
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-04-15 03:15:16 -0500

    Get participants Ok

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
 import joi from "joi";
+import dayjs from "dayjs";
 
 const app = express();
 const PORT = 5000;
@@ -34,15 +35,33 @@ app.post("/participants", async(req, res) =>{
 	}
 	try{
 		const nameDuplicated = await db.collection("participants").findOne({name:name});
-		
+
 		if (nameDuplicated) return res.status(409).send("Esse nome já está em uso");
 
 		await db.collection("participants").insertOne({name, lastStatus: Date.now()});
+
+		await db.collection("messages").insertOne({ 
+			from: name, 
+			to: "Todos", 
+			text: "entra na sala...", 
+			type: "status", 
+			time: dayjs().format("HH:mm:ss") });
+
 		res.sendStatus(201);
 
 	}catch(err){
 		res.status(500).send(err.message);
 	}
 });
+
+app.get("/participants", async(req, res)=>{
+	try{
+		const participants = await db.collection("participants").find().toArray();
+		res.send(participants);
+	}catch(err){
+		res.status(500).send(err.message);
+	}
+});
+
 
 app.listen(PORT, ()=>print(`Server online in port: ${PORT}`));
