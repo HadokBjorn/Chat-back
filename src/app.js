a96faf32ad4105843bb94ce9e0bf60652789236e
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-04-15 14:05:32 -0500

    Post messages ok

--- src/app.js
+++ src/app.js
@@ -21,11 +21,12 @@ try{
 }
 
 const db = mongoClient.db();
+const nameSchema = joi.object({
+	name: joi.string().min(3).max(30).required()
+});
 
 app.post("/participants", async(req, res) =>{
-	const nameSchema = joi.object({
-		name: joi.string().min(3).max(30).required()
-	});
+	
 	const {name} = req.body;
 	const validation = nameSchema.validate({name});
 
@@ -69,25 +70,27 @@ app.post("/messages", async(req, res)=>{
 		text: joi.string().required(),
 		type: joi.string().required().valid("private").valid("private_message")
 	});
-	const {user} = req.headers;
+	const User = req.headers.user;
 	const {to, text, type} = req.body;
+	const userValidation = nameSchema.validate({name: User});
 	const validation = messageSchema.validate(req.body);
 
-	if(!user){
-		return res.status(422).send("Necessário envio de Usuário pelo Header");
+	if (userValidation.error) {
+		const errors = userValidation.error.details.map((detail) => detail.message);
+		return res.status(422).send(errors);
 	}
 
 	if (validation.error) {
 		const errors = validation.error.details.map((detail) => detail.message);
 		return res.status(422).send(errors);
 	}
+
 	try{
-		const existUser = await db.collection("participants").findOne({name:user});
-		print(existUser);
-		print(user);
+		const existUser = await db.collection("participants").findOne({name:User});
+		
 		if (!existUser) return res.status(404).send("Usuário não encontrado");
 		await db.collection("messages").insertOne({
-			from: user,
+			from: User,
 			to,
 			text,
 			type, 
